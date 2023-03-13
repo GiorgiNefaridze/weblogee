1acdd2cc7e7be707e964e51df9ebd14fab22813d
Giorgi Nefaridze <nefogio1@gmail.com>
2023-03-14 00:37:26 +0400

    remove limiter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -14,10 +14,10 @@ import { register, login, getUserData } from "../controllers/userController.js";
 const router = Router();
 
 //Regitser user route
-router.post("/register", limiter, register);
+router.post("/register", register);
 
 //Login user route
-router.post("/login", limiter, login);
+router.post("/login", login);
 
 //Get user data
 router.post("/user-data", getUserData);
